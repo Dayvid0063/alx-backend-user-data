d7aba4e64bc9b59c98fd7fc78a3941630d07239d
Dayvid0063 <orjidavid63@gmail.com>
2024-07-03 23:52:36 +0100

    filtered fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -33,7 +33,7 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        super().__init__(self.FORMAT)
+        super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
@@ -42,7 +42,7 @@ class RedactingFormatter(logging.Formatter):
         Returns:
             str: The formatted and redacted log message.
         """
-        message = super().format(record)
+        message = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION,
                             message, self.SEPARATOR)
 
@@ -63,6 +63,13 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """Establishes and returns a connection to the database"""
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME', "root")
+    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD', "")
+    host = os.getenv('PERSONAL_DATA_DB_HOST', "localhost")
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    return mysql.connector.connect(user=user, password=passwd,
+                                   host=host, database=db_name)
 
 
 def main():
