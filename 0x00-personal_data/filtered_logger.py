0a88b7d8861c8179e751da6f3cdbfc19a25ab1b3
Dayvid0063 <orjidavid63@gmail.com>
2024-07-03 23:54:30 +0100

    filtered update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -33,7 +33,7 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        super().__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
@@ -42,7 +42,7 @@ class RedactingFormatter(logging.Formatter):
         Returns:
             str: The formatted and redacted log message.
         """
-        message = super(RedactingFormatter, self).format(record)
+        message = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
                             message, self.SEPARATOR)
 
