6b3b9443fb0500fd3a7b05b8cbca72a723853eb5
Dayvid0063 <orjidavid63@gmail.com>
2024-07-16 13:33:22 +0100

    db

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+
+"""DB module"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from user import Base, User
+
+
+class DB:
+    """DB class"""
+    def __init__(self) -> None:
+        """Initialize a new DB instance"""
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object"""
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a new user to the database and return the User object"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyword arg and return the User object"""
+        usrs = self._session.query(User)
+        for u, v in kwargs.items():
+            if u not in User.__dict__:
+                raise InvalidRequestError
+            for usr in usrs:
+                if getattr(usr, u) == v:
+                    return usr
+        raise NoResultFound
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user's attributes and commit the changes to the database"""
+        user = self.find_user_by(id=user_id)
+        for u, v in kwargs.items():
+            if not hasattr(user, u):
+                raise ValueError(f"Invalid attribute: {u}")
+            setattr(user, u, v)
+        self._session.commit()
